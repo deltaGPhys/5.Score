4c63266a5880b9a33e3f8d6be20c47b15ca04a96
Josh Gates <cty.jgates@gmail.com>
2020-01-26 19:18:59 -0500

    jwt dependency fix

--- src/main/java/com/fivedotscore/climbscore/authentication/JWTProvider.java
+++ src/main/java/com/fivedotscore/climbscore/authentication/JWTProvider.java
@@ -3,13 +3,11 @@ package com.fivedotscore.climbscore.authentication;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.security.Key;
 
 @Service
 public class JWTProvider {
